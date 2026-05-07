80ad4c0da33a6b42f7ed9728ee49c9cd6556c25d
Arvind <arvindsudarshan18@gmail.com>
2026-05-07 14:55:00 -0700

    removed div

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,10 +2,7 @@ import Terminal from "./components/Terminal"
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#0d1117] flex items-center justify-center">
-      {/* <p className="text-[#cdd9e5] font-mono">Terminal loading...</p> */}
       <Terminal />
-    </div>
   )
 }
 
